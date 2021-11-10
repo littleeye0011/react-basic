ef1e9e836181183bee6f7e7c7a099eddde426dac
littleeye0011 <eiemily2014@gmail.com>
2021-11-10 14:00:43 +0700

    add darkmode

---
+++ dark-mode/src/components/Content.js
@@ -0,0 +1,19 @@
+import light from "../image/light.svg";
+import dark from "../image/dark.svg";
+import { useContext } from "react";
+import { ThemeContext } from "../App";
+
+const Content = () => {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <main className={theme === "dark" ? "dark" : "light"}>
+      <div>
+        <h1>Test Dark Mode</h1>
+        <p>Dark Mode Workshop</p>
+      </div>
+      <img src={theme === "dark" ? dark : light} alt="logo" />
+    </main>
+  );
+};
+
+export default Content;
